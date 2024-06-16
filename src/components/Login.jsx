b1849da783e707050988698bebabcf879608660e
juancif <cifuenteslozanojuandavid@gmail.com>
2024-06-16 11:45:14 -0500

    tercer commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    if (email === 'miguelangelbeltranrache11@gmail.com' && password === 'miguel123') {
+    if (email === 'miguelangelbeltranrache11@gmail.com' && password === 'miguel123','juan@gmail.com' && password === 'juan123') {
       localStorage.setItem('authenticated', 'true');
       localStorage.setItem('userEmail', email);  // Almacenar el correo del usuario
       navigate('/dashboard');
